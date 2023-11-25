381e9e1253de9e2cbdb2a994a985fd765657c0a5
Remospo <brent.opsomer@gmail.com>
2023-11-25 01:30:29 +0100

    d

--- js/realisaties.js
+++ js/realisaties.js
@@ -64,34 +64,34 @@ function showSlidesSanitair(n) {
 }
 
 
-// let slideIndexKoeling = 1;
-// showSlidesKoeling(slideIndexKoeling);
+let slideIndexKoeling = 1;
+showSlidesKoeling(slideIndexKoeling);
 
-// function plusSlides2(n) {
-// showSlidesKoeling(slideIndexKoeling += n);
-// }
+function plusSlides2(n) {
+  showSlidesKoeling(slideIndexKoeling += n);
+}
 
-// function huidigeDia2(n) {
-// showSlidesKoeling(slideIndexKoeling = n);
-// }
+function huidigeDia2(n) {
+  showSlidesKoeling(slideIndexKoeling = n);
+}
 
-// function showSlidesKoeling(n) {
-// let i;
-// let slides = document.getElementsByClassName("koeling");
-// let dots = document.getElementsByClassName("koel");
-// if (n > slides.length) {
-//   slideIndexKoeling = 1
-// }
-// if (n < 1) {
-//   slideIndexKoeling = slides.length
-// }
-// for (i = 0; i < slides.length; i++) {
-//   slides[i].style.display = "none";
-// }
-// for (i = 0; i < dots.length; i++) {
-//   dots[i].className = dots[i].className.replace(" active", "");
-// }
-// slides[slideIndexKoeling - 1].style.display = "block";
-// dots[slideIndexKoeling - 1].className += " active";
-// // captionText.innerHTML = dots[slideIndex-1].alt;
-// }
+function showSlidesKoeling(n) {
+  let i;
+  let slides = document.getElementsByClassName("koeling");
+  let dots = document.getElementsByClassName("koel");
+  if (n > slides.length) {
+    slideIndexKoeling = 1
+  }
+  if (n < 1) {
+    slideIndexKoeling = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexKoeling - 1].style.display = "block";
+  dots[slideIndexKoeling - 1].className += " active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
+}
